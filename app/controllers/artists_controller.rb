c563fce084f60662e033eac347214158a8862dbc
Yvonne M Pham <yvonnersz@gmail.com>
2020-06-19 04:10:05 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,11 @@
 class ArtistsController < ApplicationController
+
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -47,4 +50,5 @@ class ArtistsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
 end
